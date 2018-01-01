2e7353b882ef15da3f883f13f97051a70baf4a1c
Kevin MATHY <kmathy@adneom.com>
2018-01-01 15:02:54 +0100

    make StoreDevtoolModule optional

--- templates/_store-reduxor.module.ts
+++ templates/_store-reduxor.module.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers, metaReducers } from './app.store';
 import { AllEffects } from './all-effects';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,9 +13,9 @@ import { {{ properCase name }}Service } from './{{ folder name "services" }}/{{ 
         HttpClientModule,
         StoreModule.forRoot(reducers, { metaReducers }),
         EffectsModule.forRoot([...AllEffects]),
-        StoreDevtoolsModule.instrument({
-            maxAge: 25, //  Retains last 25 states
-        })
+        // StoreDevtoolsModule.instrument({
+        //     maxAge: 25, //  Retains last 25 states
+        // })
     ],
     exports: [],
     providers: [
